510e1233db628a4d537e5b7e31cae1e793d22a45
Juan Sanchez <l22020825@veracruz.tecnm.mx>
2024-02-20 15:39:55 -0600

    Adicion al Paquete Visual de FRMProductos y FRMCiudades-V17

---
+++ src/main/java/org/example/visual/FRMCiudades.java
@@ -0,0 +1,17 @@
+package org.example.visual;
+
+import javax.swing.*;
+
+public class FRMCiudades {
+    private JPanel panel1;
+    private JButton estadosButton;
+    private JButton ciudadesButton;
+    private JButton proveedoresButton;
+    private JButton clientesButton;
+    private JButton productosButton;
+    private JButton registrarButton;
+    private JButton limpiarButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JComboBox comboBox1;
+}
---
+++ src/main/java/org/example/visual/FRMProductos.java
@@ -0,0 +1,19 @@
+package org.example.visual;
+
+import javax.swing.*;
+
+public class FRMProductos {
+    private JPanel panel1;
+    private JButton estadosButton;
+    private JButton ciudadesButton;
+    private JButton proveedoresButton;
+    private JButton clientesButton;
+    private JButton productosButton;
+    private JButton registrarButton;
+    private JButton limpiarButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JComboBox comboBox1;
+    private JRadioButton disponibleRadioButton;
+    private JRadioButton fragilRadioButton;
+}
