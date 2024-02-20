0282f01c54f64dd8dfdbfe67d42f3dd0a5278f56
Juan Sanchez <l22020825@veracruz.tecnm.mx>
2024-02-20 01:09:16 -0600

    Adicion de Paquete Visual y Creacion de FRMEStados-V15

---
+++ src/main/java/org/example/visual/FRMEstados.java
@@ -0,0 +1,17 @@
+package org.example.visual;
+
+import javax.swing.*;
+
+public class FRMEstados {
+    private JPanel panel1;
+    private JButton ciudadesButton;
+    private JButton proveedoresButton;
+    private JButton clientesButton;
+    private JButton productosButton;
+    private JButton estadosButton;
+    private JButton limpiarButton;
+    private JButton registrarButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JComboBox comboBox1;
+}
